6b9b205988e30822f63bbb3c9d9a5553e16995c4
fajartc02 <fajartc02@gmail.com>
2018-04-26 12:29:56 +0700

    improve loopingAsterisk.js

--- loopingAsterisk.js
+++ loopingAsterisk.js
@@ -1,24 +1,28 @@
 var row1 = 5;
 
- for (var i=1; i<=row1; i++) {
-     console.log('*');
-     console.log('\n')
- }
+for (var i=1; i<=row1; i++) {
+    console.log('*');
+}
+    console.log('\n')
 
 //nested loop(barisan bintang)
- var row2 = 5;
- for (var i=1; i<=row2;i++) {
-     for (var j=1; j<=row2; j++) {
-         console.log('*');
-     }
-     console.log('\n')
- }
+var row2 = 5;
+for (var i=1; i<=row2;i++) {
+  var bintang ='';
+    for (var j=1; j<=row2; j++) {
+        bintang += '*';
+    }
+    // var row2 = 0;
+      console.log(bintang);
+}
+    console.log('\n')
 
 //nested loop(barisan tangga bintang)
 var row3 = 5;
-for (var i=1; i<=row3; i++) {
+for (var i=1; i<=row2;i++) {
+  var bintang ='';
     for (var j=1; j<=i; j++) {
-        console.log('*')
+        bintang += '*';
     }
-    console.log('\n');
+      console.log(bintang);
 }
