915592e5bea1f9fe0af840fe56c36699675641b4
fajartc02 <fajartc02@gmail.com>
2018-04-27 13:56:33 +0700

    submit tugas hari jum'at

---
+++ balikKata.js
@@ -0,0 +1,17 @@
+function balikKata(kata) {
+    var row = kata.length - 1;
+    for (var i = 1; i <= 1;i++) {
+          var kosong = '';
+          for(var j = row; j >= 0; j--) {
+            kosong += kata[j];
+          }
+    }
+    return kosong;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
